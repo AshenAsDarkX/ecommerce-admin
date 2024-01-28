ef5af3578165ecb3cbe941303488139127f63d35
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-01-28 17:48:42 +0530

    Uploading images func

--- components/productForm.js
+++ components/productForm.js
@@ -1,18 +1,22 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Spinner from "./spinner";
+import { ReactSortable } from "react-sortablejs";
 
 export default function ProductForm({
   _id,
   title: exsistingTitle,
   description: ExsistingDescription,
   price: existingPrice,
-  images,
+  images: existingImages,
 }) {
   const [title, setTitle] = useState(exsistingTitle || "");
   const [description, setDescription] = useState(ExsistingDescription || "");
   const [price, setPrice] = useState(existingPrice || "");
+  const [images, setImages] = useState(existingImages || []);
   const [goToProduct, setGoToProduct] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
   const router = useRouter();
 
   if (goToProduct) {
@@ -29,29 +33,39 @@ export default function ProductForm({
       />
       <label>Photos</label>
       <div>
-        {!images?.length && (
-          <div className="mb-2">
-            <label className="w-24 h-24 border text-center flex flex-col items-center justify-center text-gray-400 rounded-md bg-slate-200 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
-                />
-              </svg>
-              Upload
-              <input type="file" className="hidden" onChange={uploadImages} />
-            </label>
-            No photos in this product
-          </div>
-        )}
+        <div className="mb-2 flex flex-wrap gap-2">
+          <ReactSortable list={images} className="flex flex-wrap gap-2" setList={updateImagesOrder}>
+            {!!images?.length &&
+              images.map((link) => (
+                <div key={link} className="h-24">
+                  <img src={link} alt="" className="rounded-md" />
+                </div>
+              ))}
+          </ReactSortable>
+          {isUploading && (
+            <div className="h-24 flex items-center">
+              <Spinner />
+            </div>
+          )}
+          <label className="w-24 h-24 border text-center flex flex-col items-center justify-center text-gray-400 rounded-md bg-slate-200 cursor-pointer">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+              />
+            </svg>
+            Upload
+            <input type="file" className="hidden" onChange={uploadImages} />
+          </label>
+        </div>
       </div>
       <label>Description</label>
       <textarea
@@ -74,7 +88,7 @@ export default function ProductForm({
 
   async function saveProduct(e) {
     e.preventDefault();
-    const data = { title, description, price };
+    const data = { title, description, price, images };
 
     if (_id) {
       await axios.put("/api/products", { ...data, _id });
@@ -86,16 +100,21 @@ export default function ProductForm({
 
   async function uploadImages(e) {
     const files = e.target?.files;
-    if (files.length > 0) {
+    if (files?.length > 0) {
+      setIsUploading(true);
       const data = new FormData();
-      for(const file of files){
-        data.append('file', file)
+      for (const file of files) {
+        data.append("file", file);
       }
-      const res = await fetch('/api/upload', {
-          method: 'POST',
-          body: data,
-      })
-      console.log(res);
+      const res = await axios.post("/api/upload", data);
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
+      });
+      setIsUploading(false);
     }
+  }
+
+  function updateImagesOrder(images) {
+    setImages(images)
   }
 }
---
+++ components/spinner.js
@@ -0,0 +1,7 @@
+import { BounceLoader, PuffLoader } from "react-spinners";
+
+export default function Spinner(){
+    return (
+        <PuffLoader size={35} color="#1F2937" speedMultiplier={2}/>
+    )
+}
